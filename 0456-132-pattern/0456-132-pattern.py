c8ba0e5d43c840570bfd6953dc7ac4b04c1047a0
lkjearl <111695405+lkjearl@users.noreply.github.com>
2023-09-30 18:48:42 +0800

    Time: 373 ms (87.26%), Space: 36.7 MB (10.8%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.py
+++ 0456-132-pattern/0456-132-pattern.py
@@ -1,14 +1,13 @@
 class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
-        n = len(nums)
-        if n < 3:
+        if len(nums) < 3:
             return False
 
         stack = []
         # '2' in 132
         secondNum = float('-inf')
 
-        for i in range(n - 1, -1, -1):
+        for i in range(len(nums) - 1, -1, -1):
             if nums[i] < secondNum:
                 return True
 
